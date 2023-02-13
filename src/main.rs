eead589f1264badbfe33432ad4af9d1fcd3cd560
JDN89 <jan.de.niels@gmail.com>
2023-02-13 23:19:43 +0100

    feat: test

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 use zero2prod::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-// Bubble up the io::Error if we failed to bind the address
-// Otherwise call .await on our Server
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
     run()?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -6,20 +6,20 @@
 // `cargo expand --test health_check` (<- name of the test file)
 #[tokio::test]
 async fn health_check_works() {
-// Arrange
+    // Arrange
     // No .await, no .expect
     spawn_app();
-// We need to bring in `reqwest`
-// to perform HTTP requests against our application.
+    // We need to bring in `reqwest`
+    // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
-// Act
+    // Act
     let response = client
         .get("http://127.0.0.1:8000/health_check")
         .send()
         .await
         .expect("Failed to execute request.");
 
-// Assert
+    // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
@@ -30,8 +30,8 @@ async fn health_check_works() {
 // all the things.
 fn spawn_app() {
     let server = zero2prod::run().expect("Failed to bind address");
-// Launch the server as a background task
-// tokio::spawn returns a handle to the spawned future,
-// but we have no use for it here, hence the non-binding let
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
     let _ = tokio::spawn(server);
 }
